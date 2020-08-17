8ea5dbebc6e0318e8332be18ebc1fe569f8f1314
pontakornth <pontakorn61@gmail.com>
2020-08-17 22:02:27 +0700

    feat: Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Fudo Ipsum</h1>
+    <span>Lorem Ipsum for people of culture</span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class App extends Vue {}
 </script>
 
