3ce8670ce76a4361d90f0df55c6a0ee390bfdde6
pontakornth <pontakorn61@gmail.com>
2020-08-19 21:54:42 +0700

    feat: Styling and adding button

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,19 @@
     <h1 class="heading">Fudo Ipsum</h1>
     <span>Lorem Ipsum for people of culture</span>
     <form class="selection-form">
-        <label for="types">Select types you need</label>
-        <input type="checkbox" value="Waifu"><label>Waifu</label>
-        <input type="checkbox" value="Husbando"><label>Husbando</label>
-        <input type="checkbox" value="WaifuHusbando"><label>WaifuHusbando</label>
+        <label class="label-heading" for="types">Select types you need</label>
+        <div class="checkboxes">
+          <label class="label-checkbox"><input type="checkbox" value="Waifu">
+              <span class="label-text">Waifu</span>
+          </label>
+          <label class="label-checkbox"><input type="checkbox" value="Husbando">
+              <span class="label-text">Husbando</span>
+          </label>
+          <label class="label-checkbox"><input type="checkbox" value="WaifuHusbando">
+              <span class="label-text">WaifuHusbando</span>
+          </label>
+        </div>
+        <button class="button">Generate</button>
     </form>
   </div>
 </template>
@@ -33,4 +42,42 @@ export default class App extends Vue {}
 .description {
   @apply text-sm;
 }
+
+.selection-form {
+  @apply p-4 border border-green-200 rounded flex flex-col justify-center items-center w-1/2;
+  @apply mx-auto;
+}
+
+.selection-form * {
+  @apply my-2;
+}
+
+.checkboxes {
+  @apply w-1/2;
+}
+
+.checkboxes * {
+  @apply my-0;
+}
+
+.label-heading {
+  @apply font-bold py-4 w-1/2;
+}
+
+.label-checkbox {
+  @apply text-left w-full block;
+}
+
+.label-text {
+  @apply p-2;
+}
+
+.button {
+  @apply p-2 border border-green-100 bg-green-600 text-white;
+}
+
+.button:hover {
+  @apply bg-green-500;
+}
+
 </style>
