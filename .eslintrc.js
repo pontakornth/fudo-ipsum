2c183c6386335b3d71e54a3a0631fe76307842c8
pontakornth <pontakorn61@gmail.com>
2020-08-27 22:30:08 +0700

    feat: Allow unused with underscore

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      varsIgnorePattern: '^_',
+    }],
   },
 };
