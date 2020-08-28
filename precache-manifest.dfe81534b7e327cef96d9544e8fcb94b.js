5a79b283f385c3da5854835f9b4e650392b49a80
pontakornth <37771900+pontakornth@users.noreply.github.com>
2020-08-28 08:10:38 +0000

    Deploying to gh-pages from  @ 5f467cab9e15cb566b6ef75dd4ef2e1ad66618aa 🚀

---
+++ precache-manifest.dfe81534b7e327cef96d9544e8fcb94b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac35eadde11dbee1b9c",
+    "url": "/css/app.82755c01.css"
+  },
+  {
+    "revision": "2d1042e6177f16ff2a1127978f1d4f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac35eadde11dbee1b9c",
+    "url": "/js/app.9f3825f4.js"
+  },
+  {
+    "revision": "e3199c823a22b4490113",
+    "url": "/js/chunk-vendors.e72d2095.js"
+  },
+  {
+    "revision": "14ececf5959f771d3666a276853ccbe8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
